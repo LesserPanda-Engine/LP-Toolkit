1eee315c31cd3fb0bc4eeeeaf9991b6996ca0704
Sean Bohan <pixelpicosean@gmail.com>
2015-08-16 20:26:05 +0800

    Change package info and rename panda with panda.

--- lpanda.js
+++ lpanda.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-var pandajs = require('./index');
+var lpanda = require('./index');
 var command = process.argv[2];
 
-if (!pandajs[command]) command = 'help';
+if (!lpanda[command]) command = 'help';
 
-pandajs[command](process.cwd(), function(err) {
+lpanda[command](process.cwd(), function(err) {
     if (err) console.log(err);
     else console.log('Done');
 }, process.argv.splice(3));
